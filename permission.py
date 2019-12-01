d4b6ecd9636f6b9bf754442eb61eaf3e69c814d8
dutr5288 <dutr5288@colorado.edu>
2019-11-30 19:32:39 -0700

    Added: Permission Group

--- permission.py
+++ permission.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Permission
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
+        print(request.user)
         print(request.user.groups)
         if request.user and request.user.groups.filter(name='LIB-Counter-Reports').exists():
             return True
